f07c3354a800074c22978297a53bc187adeb9423
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-07-12 15:17:35 -0400

    Tabla Pet poblada + fotos en carpeta

--- script.sql
+++ script.sql
@@ -3,6 +3,18 @@ CREATE DATABASE unhogarmas;
 
 CREATE TABLE pets (id SERIAL PRIMARY KEY, name VARCHAR(20) NOT NULL, author_post INT REFERENCES users(id), specie VARCHAR(20) NOT NULL, age int NOT NULL, weight INT NOT NULL, gender VARCHAR(10), chip BOOLEAN NOT NULL, photo VARCHAR(255) NOT NULL, description VARCHAR (255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
+INSERT INTO pets (name, author_post, specie, age, weight, gender, chip, photo, description)
+VALUES 
+('Luna', 1, 'perro', 2, 4, 'female', true, 'luna.jpg', 'Tranquila, ideal para departamentos.'),
+('Rayo', 2, 'gato', 0, 2, 'male', false, 'rayo.jpg', 'Juguetón y curioso, necesita compañía.'),
+('Simón', 1, 'conejo', 5, 12, 'male', true, 'simon.jpg', 'Amigable y bien entrenado.'),
+('Mora', 3, 'gato', 3, 6, 'female', true, 'mora.jpg', 'Sociable con niños y otras mascotas.'),
+('Toby', 2, 'perro', 7, 18, 'male', false, 'toby.jpg', 'Enérgico y protector, busca espacio.'),
+('Lila', 4, 'conejo', 1, 3, 'female', false, 'lila.jpg', 'Muy cariñosa, le encanta dormir al sol.'),
+('Nico', 3, 'perro', 4, 25, 'male', true, 'nico.jpg', 'Leal y obediente, necesita ejercicio.'),
+('Brisa', 1, 'gato', 2, 5, 'female', true, 'brisa.jpg', 'Independiente y silenciosa, ideal para adultos.');
+
+
 CREATE TABLE users (id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, rut VARCHAR(12) UNIQUE NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, photo VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE request ( id SERIAL PRIMARY KEY, age INT NOT NULL,  phone VARCHAR(20) NOT NULL, address VARCHAR(255) NOT NULL, housing_type VARCHAR(50), allows_pets BOOLEAN NOT NULL, pet_name VARCHAR(100), reason TEXT, household VARCHAR(100), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, user_id INT REFERENCES users(id), status VARCHAR(10) CHECK (status IN ('pendiente', 'aceptada')) DEFAULT 'pendiente'
