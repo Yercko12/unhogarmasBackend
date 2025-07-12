1a7f25f0c91ce10bf7d92769b35579a70c26d6f8
yercko12 <talamilla.yercko@gmail.com>
2025-07-12 12:05:16 -0400

    Se  elimina authcontroller, se agrega usercontroller(CRUD y login/register) se crea carpeta de middleware, ademas se instala bscrypt y JWT

---
+++ controllers/pet.controller.js
@@ -0,0 +1,104 @@
+import { petModel } from "../models/pet.model.js";
+
+//lo que se vera en la pagina
+const read = async (req, res) => {
+  const { limit = 8, order = "ASC", page = 1 } = req.query;
+  const isPageValid = /^[1-9]\d*$/.test(page);
+
+  if (!isPageValid) {
+    return res.status(400).json({ message: "Invalid page number, page > 0" });
+  }
+
+  try {
+    const pets = await petModel.findAll({
+      limit,
+      order,
+      page,
+      user: req.user,
+    });
+    return res.json(pets);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+//leer las mascotas por medio del ID
+const readById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const pet = await petModel.findById(id);
+    if (!pet) {
+      return res.status(404).json({ message: "Mascota no encontrada" });
+    }
+    return res.json(pet);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+//crear la mascota
+const create = async (req, res) => {
+  const { name, breed, age, gender, chip, photo, description } = req.body;
+
+  if (!name || !breed || age || !gender || !chip || !photo || !description) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  const newPet = {
+    name,
+    breed,
+    age,
+    gender,
+    chip,
+    photo,
+    description,
+  };
+
+  try {
+    const createdPet = await petModel.create(newPet);
+    return res.status(201).json(createdPet);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+//actualizar perfil de la mascota
+const update = async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+
+  try {
+    const updatedPet = await petModel.update(id, updateData);
+    if (!updatedPet) {
+      return res.status(404).json({ message: "Mascota no encontrada" });
+    }
+    return res.json(updatedPet);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+//eliminar mascota
+const remove = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedPet = await petModel.remove(id);
+    if (!deletedPet) {
+      return res.status(404).json({ message: "Mascota no encontrada" });
+    }
+    return res.json({ message: "Mascota eliminada" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+
+export const petController = {
+  read,
+  readById,
+  create,
+  update,
+  remove,
+};
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,75 @@
-import { userModel } from '../models/user.model'; 
+import { userModel } from '../models/user.model';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 
+const SECRET_KEY = process.env.JWT_SECRET || 'clave_secreta';
+const JWT_EXPIRES_IN = '1h';
+
+//Registro del usuario
+const register = async (req, res) => {
+    const { firstName, lastName, email, rut, password, photo } = req.body;
+
+    if (!firstName || !lastName || !email || !rut || !password) {
+        return res.status(400).json({ message: 'Todos los campos son requeridos' });
+    }
+
+    try {
+        const userExist = await userModel.findByEmail(email);
+        if (userExist) {
+            return res.status(409).json({ message: 'El correo electrónico ya está en uso' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await userModel.create({
+            firstName,
+            lastName,
+            email,
+            rut,
+            photo,
+            password: hashedPassword,
+        });
+
+        return res.status(201).json({ message: 'Usuario creado exitosamente', user: newUser });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error en el servidor' });
+    }
+};
+//login del usuario
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Correo electrónico y contraseña requeridos' });
+    }
+
+    try {
+        const user = await userModel.findByEmail(email);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ message: 'Credenciales incorrectas' });
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email, role: user.userRole }, SECRET_KEY, { expiresIn: '2h' });
+
+        return res.json({ message: 'Login exitoso', token, user });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Error en el servidor' });
+    }
+};
+
+
+
+
+//leer la informacion mediante el ID
 const readById = async (req, res) => {
     const id = req.params.id;
     try {
@@ -22,16 +90,20 @@ const create = async (req, res) => {
     if (!firstName || !lastName || !email || !password || !rut) {
         return res.status(400).json({ message: "Todos los campos son requeridos" });
     }
-    const newUser = {
-        firstName,
-        lastName,
-        email,
-        password,
-        rut,
-        photo: photo,
-    };
 
     try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = {
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            rut,
+            photo: photo,
+        };
+
+
         const user = await userModel.create(newUser);
         return res.status(201).json(user);
     } catch (error) {
@@ -40,6 +112,7 @@ const create = async (req, res) => {
     }
 };
 
+//actualizar informacion del usuario 
 const update = async (req, res) => {//preguntar acerca punto 40 a 54//
     const id = req.params.id;
     const userData = req.body;
@@ -56,8 +129,10 @@ const update = async (req, res) => {//preguntar acerca punto 40 a 54//
     }
 }
 
+
 export const userController = {
     readById,
     create,
     update,
+    register,
 };
